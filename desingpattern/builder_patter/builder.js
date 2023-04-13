9aaa41bb6f9125864cd705ed8059fcf3de8f2e9b
Maikon <m.carvalho@grouplinknetwork.com>
2023-04-13 02:06:39 -0300

    Add Builde patterns

---
+++ desingpattern/builder_patter/builder.js
@@ -0,0 +1,47 @@
+class House {
+    constructor(builder) {
+        this.bedrooms = builder.bedrooms;
+        this.bathrooms = builder.bathrooms;
+        this.kitchens = builder.kitchens;
+        this.garages = builder.garages;
+    }
+}
+
+class HouseBuilder {
+    constructor() {
+        this.bedrooms = 0;
+        this.bathrooms = 0;
+        this.kitchens = 0;
+        this.garages = 0;
+    }
+
+    setBedRooms(bedrooms) {
+        this.bedrooms = bedrooms
+        return this
+    }
+    setKitchens(kitchens) {
+        this.kitchens = kitchens
+        return this;
+    }
+
+    setBathrooms(bathrooms) {
+        this.bathrooms = bathrooms
+        return this
+    }
+    setGarage(garages) {
+        this.garages = garages
+        return this
+    }
+    build() {
+    return new House(this);
+    }
+}
+
+const house1 = new HouseBuilder()
+    .setBedRooms(3)
+    .setKitchens(1)
+    .setBathrooms(1)
+    .setGarage(1)
+    .build();
+
+console.log(house1)
---
+++ desingpattern/builder_patter/readme.md
@@ -0,0 +1,5 @@
+## Builder Patterin 
+
+The focus is to separe construction of complex Objects from ther representations. In Node Js
+the way the create objects is step-by-step manner
+
